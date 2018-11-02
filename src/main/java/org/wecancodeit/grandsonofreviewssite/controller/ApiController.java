0bceb7af0023a56a0e4ee195244ff00ff6f6d1e5
WaynePL <wayneplivingston@gmail.com>
2018-11-02 14:27:06 -0400

    tags are almost there

--- src/main/java/org/wecancodeit/grandsonofreviewssite/controller/ApiController.java
+++ src/main/java/org/wecancodeit/grandsonofreviewssite/controller/ApiController.java
@@ -1,10 +1,14 @@
 package org.wecancodeit.grandsonofreviewssite.controller;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.grandsonofreviewssite.model.Tag;
+import org.wecancodeit.grandsonofreviewssite.repository.ReviewRepository;
 import org.wecancodeit.grandsonofreviewssite.repository.TagRepository;
 
 @RestController
@@ -13,8 +17,11 @@ public class ApiController {
 	@Autowired
 	TagRepository tagRepo;
 
+	@Autowired
+	ReviewRepository reviewRepo;
+
 	@GetMapping("/api/reviews/{id}")
-	public Tag getTagsForReview(@PathVariable(value = "id") Long id) {
-		return tagRepo.findById(id).get();
+	public Collection<Tag> getTagsForReview(@PathVariable(value = "id") Long id) {
+		return reviewRepo.findById(id).get().getTags();
 	}
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,28 @@
+var xhttp = new XMLHttpRequest()
+// Sets behavior for when the AJAX request is complete
+xhttp.onreadystatechange = function() {
+	// Checks the ready state and http status code
+	if (this.readyState == 4 && this.status == 200) {
+		// Typical action to be performed when the document is ready:
+		console.log("This works?");
+	}
+}
+/*
+** Opens the request and sets the type of request,
+** location of the data,
+** and whether the request is asynchronous
+*/
 
-fetch(`/api/reviews/${window.location.pathname.split('/')[2]}`)
-	.then(res => res.json())
+xhttp.open('GET', `/api/reviews/${window.location.pathname.split('/')[2]}`, true)
+// Sends the request to the server
+xhttp.send()
+
+
+/*fetch(`/api/reviews/${window.location.pathname.split('/')[2]}`)
+	.then(res => {
+		console.log(res.status)
+		res.json()
+	})
 	.then(data => {
 		const body = document.body
 		const p = document.querySelector(".tags")
@@ -10,4 +32,4 @@ fetch(`/api/reviews/${window.location.pathname.split('/')[2]}`)
 		
 		body.innerHTML += p.innerHTML
 
-	})
+	})*/
