1dcf4edb64c02567c6ba39a162038976cebfe6f0
WaynePL <wayneplivingston@gmail.com>
2018-11-02 13:03:14 -0400

    added test for tags

--- src/test/java/org/wecancodeit/grandsonofreviewssite/repository/TagRepositoryTest.java
+++ src/test/java/org/wecancodeit/grandsonofreviewssite/repository/TagRepositoryTest.java
@@ -1,5 +1,42 @@
 package org.wecancodeit.grandsonofreviewssite.repository;
 
+import static org.hamcrest.Matchers.hasItem;
+
+import java.util.Collection;
+
+import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.wecancodeit.grandsonofreviewssite.model.Review;
+import org.wecancodeit.grandsonofreviewssite.model.Tag;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class TagRepositoryTest {
 
+	@Resource
+	TagRepository tagRepo;
+
+	@Test
+	public void shouldAddATag() {
+		Tag tag1 = new Tag("tag");
+		tagRepo.save(tag1);
+
+		Iterable<Tag> result = tagRepo.findAll();
+		Assert.assertThat(result, hasItem(tag1));
+
+	}
+
+	public void shouldAddATagToReview() {
+		Tag tag1 = new Tag("tag");
+
+		Review review = new Review();
+		review.addTag(tag1);
+
+		Assert.assertThat(review.getTags(), hasItem(tag1));
+	}
 }
